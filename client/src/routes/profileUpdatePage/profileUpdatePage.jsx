e219c7d70fcff78929ab8b609d4ca239011c1d97
AdrianRMV <adrianthelawx@gmail.com>
2024-05-26 00:11:57 -0700

    profile update api completed

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,18 +1,48 @@
+import { useContext, useState } from 'react';
 import './profileUpdatePage.scss';
+import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
 
 function ProfileUpdatePage() {
+    const [error, setError] = useState('');
+    const { updateUser, currentUser } = useContext(AuthContext);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        const { username, email, password } = Object.fromEntries(formData);
+
+        try {
+            const res = apiRequest.put(`/users/${currentUser.id}`);
+        } catch (error) {
+            console.log(error);
+            setError(error.response.data.message);
+        }
+    };
+
     return (
         <div className="profileUpdatePage">
             <div className="formContainer">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>Update Profile</h1>
                     <div className="item">
                         <label htmlFor="username">Username</label>
-                        <input id="username" name="username" type="text" />
+                        <input
+                            id="username"
+                            name="username"
+                            type="text"
+                            defaultValue={currentUser.username}
+                        />
                     </div>
                     <div className="item">
                         <label htmlFor="email">Email</label>
-                        <input id="email" name="email" type="email" />
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            defaultValue={currentUser.email}
+                        />
                     </div>
                     <div className="item">
                         <label htmlFor="password">Password</label>
@@ -22,7 +52,11 @@ function ProfileUpdatePage() {
                 </form>
             </div>
             <div className="sideContainer">
-                <img src="" alt="" className="avatar" />
+                <img
+                    src={currentUser.avatar || '/noavatar.jpg'}
+                    alt=""
+                    className="avatar"
+                />
             </div>
         </div>
     );
