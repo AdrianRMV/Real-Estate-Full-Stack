0845f3e18f75e92bd0160b6248badda9f0e82138
AdrianRMV <adrianthelawx@gmail.com>
2024-05-29 15:55:29 -0700

    Posts api done

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,21 +1,19 @@
 import express from 'express';
+import { verifyToken } from '../middleware/verifyToken.js';
+import {
+    addPost,
+    delatePost,
+    getPost,
+    getPosts,
+    updatePost,
+} from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-// router.get('/test', (req, res) => {
-//     console.log('HOLA');
-// });
-
-// router.post('/test', (req, res) => {
-//     console.log('HOLA');
-// });
-
-// router.put('/test', (req, res) => {
-//     console.log('HOLA');
-// });
-
-// router.delete('/test', (req, res) => {
-//     console.log('HOLA');
-// });
+router.get('/', getPosts);
+router.get('/:id', getPost);
+router.post('/', verifyToken, addPost);
+router.put('/:id', verifyToken, updatePost);
+router.delete('/:id', verifyToken, delatePost);
 
 export default router;
